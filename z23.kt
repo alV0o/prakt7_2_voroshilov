79e2be27afd2113b1243c63736ade434d6bdd163
alV0o <voroshilov.olexandro07@mail.ru>
2025-02-20 23:32:36 +0500

    Update z23.kt

--- z23.kt
+++ z23.kt
@@ -1,8 +1,29 @@
 fun main(){
-    val x = readLine()!!.toDouble()
-    val y = readLine()!!.toDouble()
+    try {
+        println("Введите координаты точки")
+        val x0 = readLine()!!.toDouble()
+        val y0 = readLine()!!.toDouble()
 
-    val r = Math.sqrt(x*x + y*y)
+        println("Введите координаты малого круга")
+        val x1 = readLine()!!.toDouble()
+        val y1 = readLine()!!.toDouble()
+        val r = Math.sqrt(x1 * x1 + y1 * y1)
 
+        println("Введите координаты большого круга")
+        val x2 = readLine()!!.toDouble()
+        val y2 = readLine()!!.toDouble()
+        val r2 = Math.sqrt(x2 * x2 + y2 * y2)
 
+        when {
+            (r < r2) -> when {
+                (x0 * x0 + y0 * y0 <= r * r) -> println("Попал")
+                else -> println("Не попал")
+            }
+
+            else -> println("Неправильное кольцо")
+        }
+    }
+    catch (e: Exception){
+        println("неверный формат")
+    }
 }
